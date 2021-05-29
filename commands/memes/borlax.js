8e1df3caf2db3fa7183ef18455efb8f5cc45170c
Taz-Z <tanzim_zaman@hotmail.com>
2021-05-29 18:18:52 -0400

    s

---
+++ commands/memes/borlax.js
@@ -0,0 +1,7 @@
+import { command, generateEmbed } from "../../helper.js";
+
+const execute = (message, args) => {
+  message.channel.send("Literal motherfucker wannnabe");
+};
+
+export default command("borlax", execute);
---
+++ commands/memes/pops.js
@@ -0,0 +1,7 @@
+import { command, generateEmbed } from "../../helper.js";
+
+const execute = (message, args) => {
+  message.channel.send("Is currently making cancer his lil bitch");
+};
+
+export default command("pops", execute);
