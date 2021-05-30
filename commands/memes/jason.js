4ad8313eb0ac5f23d4e127f008eeb379046c5442
Taz-Z <tanzim_zaman@hotmail.com>
2021-05-29 20:32:51 -0400

    Create jason.js

---
+++ commands/memes/jason.js
@@ -0,0 +1,7 @@
+import { command, generateEmbed } from "../../helper.js";
+
+const execute = (message, args) => {
+  message.channel.send("The names Jason, Jason Jason. Organization? Jason.");
+};
+
+export default command("jason", execute);
