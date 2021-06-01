863382573d68f13ca637c4259835113e1f992b77
Taz-Z <tanzim_zaman@hotmail.com>
2021-06-01 02:16:13 -0400

    Set up multi messages

--- commands/memes/alex.js
+++ commands/memes/alex.js
@@ -1,9 +1,13 @@
-import { command, generateEmbed } from "../../helper.js";
+import { command, generateEmbed, getRandomInt } from "../../helper.js";
+
+const messageChoices = [
+  "If you meeled more than the hpriest you might be in for a fight :).",
+  "We all know the only reason you play monk is because it's one `e` away from monke.",
+  "For all the first person you played in WoW, you sure do suck at valorant.",
+];
 
 const execute = (message, args) => {
-  message.channel.send(
-    "If you meeled more than the hpriest you might be in for a fight :)."
-  );
+  message.channel.send(messageChoices[getRandomInt(0, messageChoices.length)]);
 };
 
 export default command("alex", execute);
--- helper.js
+++ helper.js
@@ -3,6 +3,12 @@ import { MessageEmbed } from "discord.js";
 export const generateEmbed = (color, title, fields) =>
   new MessageEmbed().setColor(color).setTitle(title).addFields(fields);
 
+export const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+};
+
 export const command = (
   name,
   execute,
