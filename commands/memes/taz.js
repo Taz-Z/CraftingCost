ca664ea8141a9f2071777045f8b9ae457c7fdbf6
Alex Dine <alexdine1996@gmail.com>
2021-06-01 02:45:31 -0400

    Updated taz.js
    test to multi message taz's bs

--- commands/memes/taz.js
+++ commands/memes/taz.js
@@ -1,7 +1,13 @@
-import { command, generateEmbed } from "../../helper.js";
+import { command, generateEmbed, getRandomInt } from "../../helper.js";
+
+const messageChoices = [
+	"Needs a fucking hobby!",
+	"Best streamer EUW!",
+	"I cannot stress how badly this man needs a hobby... Like seriously this was so much work just to shitpost.",
+];
 
 const execute = (message, args) => {
-  message.channel.send("Needs a fucking hobby!");
+  message.channel.send(messageChoices[getRandomInt(0, messageChoices.length)]);
 };
 
 export default command("taz", execute);
