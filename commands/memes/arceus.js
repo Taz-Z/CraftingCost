9a01c95eb576d46816faf99d764a3526fcb50762
Taz-Z <tanzim_zaman@hotmail.com>
2021-05-31 22:09:52 -0400

    Update arceus.js

--- commands/memes/arceus.js
+++ commands/memes/arceus.js
@@ -1,7 +1,7 @@
 import { command, generateEmbed } from "../../helper.js";
 
 const execute = (message, args) => {
-  message.channel.send("Thanks for ignoring our 7 invites last week, nbd.");
+  message.channel.send("Thanks for ignoring our 8* invites last week, nbd.");
 };
 
 export default command("arceus", execute);
