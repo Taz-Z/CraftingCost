192e08bdf53156f0908ea1265aa40de3fa910be8
Taz-Z <tanzim_zaman@hotmail.com>
2021-06-04 00:53:37 -0400

    oh god what have I done

--- commands/functional/stocks.js
+++ commands/functional/stocks.js
@@ -10,10 +10,9 @@ const convertAmount = async (from, to, amount) => {
 const headers = () => {
   return {
     "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
-    "X-RapidAPI-Key": "2da446beb8msh6d88186527d347cp13b59cjsndf5dd86a9d30"
-
-  }
-}
+    "X-RapidAPI-Key": "2da446beb8msh6d88186527d347cp13b59cjsndf5dd86a9d30",
+  };
+};
 
 const execute = async (message, args) => {
   message.channel.send(`Skeleton`);
--- commands/memes/arceus.js
+++ commands/memes/arceus.js
@@ -1,7 +1,17 @@
 import { command } from "../../helper.js";
 
 const execute = (message, args) => {
-  message.channel.send("Thanks for ignoring our 8* invites last week, nbd.");
+  const arceus = "<@232380548394385418>";
+  message.channel.send(`Hey ${arceus}`);
+  message.channel.send(`Thanks ${arceus}`);
+  message.channel.send(`for ${arceus}`);
+  message.channel.send(`ignoring ${arceus}`);
+  message.channel.send(`our ${arceus}`);
+  message.channel.send(`9* ${arceus}`);
+  message.channel.send(`invites ${arceus}`);
+  message.channel.send(`last ${arceus}`);
+  message.channel.send(`week ${arceus}`);
+  message.channel.send(`Nbd ${arceus}`);
 };
 
 export default command("arceus", execute);
