fa29079155380a9efe58096fa3c6b076de28adbe
Taz-Z <tanzim_zaman@hotmail.com>
2021-06-04 00:57:51 -0400

    Update arceus.js

--- commands/memes/arceus.js
+++ commands/memes/arceus.js
@@ -1,17 +1,17 @@
 import { command } from "../../helper.js";
 
-const execute = (message, args) => {
+const execute = async (message, args) => {
   const arceus = "<@232380548394385418>";
-  message.channel.send(`Hey ${arceus}`);
-  message.channel.send(`Thanks ${arceus}`);
-  message.channel.send(`for ${arceus}`);
-  message.channel.send(`ignoring ${arceus}`);
-  message.channel.send(`our ${arceus}`);
-  message.channel.send(`9* ${arceus}`);
-  message.channel.send(`invites ${arceus}`);
-  message.channel.send(`last ${arceus}`);
-  message.channel.send(`week ${arceus}`);
-  message.channel.send(`Nbd ${arceus}`);
+  await message.channel.send(`Hey ${arceus}`);
+  await message.channel.send(`Thanks ${arceus}`);
+  await message.channel.send(`for ${arceus}`);
+  await message.channel.send(`ignoring ${arceus}`);
+  await message.channel.send(`our ${arceus}`);
+  await message.channel.send(`9* ${arceus}`);
+  await message.channel.send(`invites ${arceus}`);
+  await message.channel.send(`last ${arceus}`);
+  await message.channel.send(`week ${arceus}`);
+  await message.channel.send(`Nbd ${arceus}`);
 };
 
 export default command("arceus", execute);
